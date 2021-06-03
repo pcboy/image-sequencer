0f9c02fcf6482617a0582c91edba0b2322ef0dae
pcboy <david.hagege@gmail.com>
2021-06-03 17:04:40 +0900

    Fix replace image call

--- src/ReplaceImage.js
+++ src/ReplaceImage.js
@@ -43,9 +43,9 @@ function ReplaceImage(ref, selector, steps, options) {
     function make(url) {
       tempSequencer.loadImage(url, function(){
         // this.addSteps(steps).run({stop:function(){}},function(out){
-        var sequence = this.addSteps(steps);
+        var sequence = tempSequencer.addSteps(steps);
         if (ref.detectStringSyntax(steps))
-          sequence = this.stringToSteps(steps);
+          sequence = tempSequencer.stringToSteps(steps);
         sequence.run({stop:function(){}}, function(out){
           img.src = out;
         });
